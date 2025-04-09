53e789de3289d2b335eee308601e0290ab3d55f5
kevin030-anto <kevin030is@gmail.com>
2025-04-09 15:49:51 +0530

    source code upload

---
+++ Library Management PHP/Library Management PHP/LMS/hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b></b>
+    </div>
+    <strong>Libray Management System &nbsp;Brought To You By <a href="https://github.com/kevin030-anto"> kevin030-anto</a>.</strong>
+  </footer>
